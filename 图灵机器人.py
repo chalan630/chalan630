43578fbed567ad8209c65ecbd294fc7e1c6fbb20
chalan630 <18220724360@163.com>
2017-07-29 14:01:51 +0800

    加入一个图灵机器人

---
+++ 图灵机器人.py
@@ -0,0 +1,28 @@
+import itchat
+from itchat.content import *
+import urllib
+import json
+
+@itchat.msg_register(itchat.content.TEXT)   #itchat默认
+
+
+def text_reply(msg):
+    info = msg['Text'].encode('UTF-8')
+    url = 'http://www.tuling123.com/openapi/api'
+    data = {"key":"815419f186d74f93a04061afef632783","info":info,"userid":"chalan630"}
+    data = urllib.parse.urlencode(data).encode(encoding='UTF8')
+    request = urllib.request.Request(url, data)
+    response = urllib.request.urlopen(request)
+    re_info_str = response.read()
+    re_info_dict = json.loads(re_info_str)    #str转成dict
+    print ('Reply message is:',re_info_dict)
+    if re_info_dict['code'] == 100000:  #返回数据格式为文本类
+        itchat.send(re_info_dict['text'], msg['FromUserName']) #itchat信息发送     回复信息与回复者用户名
+#   return '%s' % re_info_dict['text']
+
+itchat.auto_login(hotReload = True, enableCmdQR = 1) #登陆
+
+#mps = itchat.get_mps()
+#print(mps)
+
+itchat.run()    #运行
